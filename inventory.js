4ca766a86a86228c682d11080aa83682cacc4c68
Jordan Quigley-Jones <jordan@pusher.com>
2017-03-03 15:48:46 +0000

    Form and function to create sodas

--- inventory.js
+++ inventory.js
@@ -8,13 +8,36 @@ Parse.databaseUri = 'mongodb://heroku_wlwgjx3v:p1kehucb5fepar9dd1var490lu@ds1136
 ////////////// SODAS //////////////////////
 
 //Creates the class of sodas
-// var SupernaturalSoda = Parse.Object.extend("SupernaturalSoda");
-// // Creates the new soda
-// var supernaturalSoda = new SupernaturalSoda();
 
-// // Sets attributes of the new soda
-// supernaturalSoda.set("sodaName", "Apple");
-// supernaturalSoda.set("sodaQuantity", 4);
+var SupernaturalSoda = Parse.Object.extend("SupernaturalSoda");
+// Creates the new soda
+
+function SodaFactory(attributes) {
+	var supernaturalSoda = new SupernaturalSoda();
+	for (attribute in attributes) {
+		supernaturalSoda.set(attribute, attributes[attribute]);
+	}
+		// Sets attributes of the new soda
+	supernaturalSoda.save(null, {
+		success: function(supernaturalSoda) {
+		// Execute any logic that should take place after the object is saved.
+			console.log(supernaturalSoda);
+		},
+  		error: function(supernaturalSoda, error) {
+   		 // Execute any logic that should take place if the save fails.
+    	// error is a Parse.Error with an error code and message.
+    		console.log('Failed to create new object, with error code: ' + error.message);
+    	}
+    });		
+}
+
+var newFlavour = {
+	sodaName: "Chocolate",
+	sodaQuantity: 174
+}
+
+var newSoda = SodaFactory(newFlavour);
+
 
 // supernaturalSoda.save(null, {
 // 	success: function(supernaturalSoda) {
@@ -28,16 +51,16 @@ Parse.databaseUri = 'mongodb://heroku_wlwgjx3v:p1kehucb5fepar9dd1var490lu@ds1136
 //   }
 // });
 
-//////////// GET IT BACK //////////
-var SupernaturalSoda = Parse.Object.extend("SupernaturalSoda");
-var query = new Parse.Query(SupernaturalSoda);
-query.get("3zfG4OOtMG", {
-	success: function(supernaturalSoda) {
-		// The object was retrieved successfully
-		console.log(supernaturalSoda.get("sodaQuantity"))
-	},
-	error: function(object, error) {
-		// The object was not retrieved succesffully
-		console.log("Error: " + error.code + " " + error.message);
-	}
-});
+// //////////// GET IT BACK //////////
+// var SupernaturalSoda = Parse.Object.extend("SupernaturalSoda");
+// var query = new Parse.Query(SupernaturalSoda);
+// query.get("3zfG4OOtMG", {
+// 	success: function(supernaturalSoda) {
+// 		// The object was retrieved successfully
+// 		console.log(supernaturalSoda.get("sodaQuantity"))
+// 	},
+// 	error: function(object, error) {
+// 		// The object was not retrieved succesffully
+// 		console.log("Error: " + error.code + " " + error.message);
+// 	}
+// });
